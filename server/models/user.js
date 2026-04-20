c09165ef865b4ff0ddec7fa109cbb2b14e95b8b2
Yousuf Khalid <yousufkhalid1308@gmail.com>
2026-04-21 03:27:35 +0530

    Feat: User Model

---
+++ server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: {
+    type: String,
+    enum: ['client', 'freelancer', 'admin'],
+    default: 'client'
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
