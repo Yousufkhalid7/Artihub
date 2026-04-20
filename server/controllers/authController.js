64a04d1ba45eff01c2c2e8a7c39fba7cc632e1a6
Yousufkhalid7 <yousufkhalid1308@gmail.com>
2026-04-21 02:00:26 +0530

    Fix: Password hashing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,7 +13,7 @@ exports.register = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const user = await User.create({
-            name, email, password, role,
+            name, email, password: hashedPassword, role,
         });
         res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
@@ -39,8 +39,8 @@ exports.login = async (req, res) => {
             process.env.JWT_SECRET,
             { expiresIn: "7d" },
         );
-        res.json({token})
-    }catch(error){
-        res.status(500).json({error: error.message})
+        res.json({ token })
+    } catch (error) {
+        res.status(500).json({ error: error.message })
     }
-    }
+}
